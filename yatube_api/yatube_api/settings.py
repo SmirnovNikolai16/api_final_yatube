2c544cd55ca324796b16c12df81e64b3bee74f5e
Nikolai <smirnovnikolai@yandex.ru>
2021-07-31 22:38:11 +0300

    Удаление не нужных файлов2

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -17,7 +17,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
-    'drf_yasg',
     'rest_framework',
     'djoser',
     'rest_framework.authtoken',
